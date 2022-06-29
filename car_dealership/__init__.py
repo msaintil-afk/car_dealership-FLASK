ab2ebc0b845dc8face5ecf80a037974540f6a53d
msaintil-afk <marcus.saintil@gmail.com>
2022-06-29 07:55:34 -0500

    connected db

--- car_dealership/__init__.py
+++ car_dealership/__init__.py
@@ -1,11 +1,27 @@
 from flask import Flask
-from config import Config
 from .site.routes import site
+# from .api.routes import api
+# todo api
 from .authentication.routes import auth
+from config import Config
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from .models import db as root_db
+
 
 app = Flask(__name__)
 
+app.register_blueprint(site)
+# app.register_blueprint(api)
+# todo register api
+app.register_blueprint(auth)
+
+
+
 app.config.from_object(Config)
 
-app.register_blueprint(site)
-app.register_blueprint(auth)
+root_db.init_app(app)
+#order matters
+migrate = Migrate(app, root_db)
